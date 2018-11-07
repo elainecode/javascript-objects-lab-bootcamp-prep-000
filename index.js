eba4fa1b6ee5dfa819963e715b8e88f53eb7a1f9
inherited-architect-4472 <elaine.onb@gmail.com>
2018-11-07 07:33:48 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,9 +1,8 @@
-describe('Objects', function() {
-  it('defines a `recipes` object', function() {
-    expect(typeof recipes).toEqual('object')
-  })
 
-  describe('updateObjectWithKeyAndValue(object, key, value)', function() {
+
+var recipes = {};
+
+  function updateObjectWithKeyAndValue(object, key, value) {
     it('returns an object with the orignal key value pairs and the new key value pair', function() {
       var obj = { prop: 1 }
 
@@ -11,7 +10,7 @@ describe('Objects', function() {
         prop: 1,
         prop2: 2
       })
-    })
+    }
 
     it('it does not modify the original object, but rather returns a clone with the new data', function() {
       var obj = { prop: 1 }
