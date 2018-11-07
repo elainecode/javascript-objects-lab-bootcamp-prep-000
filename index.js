2e69f678db9cf7baf2f39e55ca7256726e15d446
inherited-architect-4472 <elaine.onb@gmail.com>
2018-11-07 07:48:09 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -13,33 +13,9 @@ var recipes = {};
 
 
 function deleteFromObjectByKey(object, key){
- Object.assign({}, object, { key: value });
+return  Object.assign({}, object, { key: ""});
+    }
 
-      expect(newObj['prop']).toBe(undefined)
-    })
-
-    it('does not modify the original object (it is non-destructive)', function() {
-      var obj = { prop: 1 }
-
-      deleteFromObjectByKey(obj, 'prop')
-      expect(obj['prop']).toBe(1)
-    })
-  })
-
-  describe('destructivelyDeleteFromObjectByKey(object, key)', function() {
-    it('returns object without the delete key/value pair', function() {
-      var obj = { prop: 1 }
-      var newObj = destructivelyDeleteFromObjectByKey(obj, 'prop');
-
-      expect(newObj['prop']).toBe(undefined)
-    })
-
-    it('modifies the original object', function() {
-      var obj = { prop: 1 }
-      var newObj = destructivelyDeleteFromObjectByKey(obj, 'prop');
-
-      expect(obj['prop']).toBe(undefined)
-    })
-  })
-
-})
+function destructivelyDeleteFromObjectByKey(object, key){
+ delete object[key];
+    }
